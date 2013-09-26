scala/scala-xml

commit b9695f013a2e57bd643e3dbb756af4a0f6a99383

Test deps conditional on TestKeys.includeTestDependencies

build.sbt
@@ -78,12 +78,18 @@ pomExtra := (
   </developers>
 )
 
-// for testing with partest
-libraryDependencies += "org.scala-lang.modules" %% "scala-partest-interface" % "0.2" % "test"
+// default value must be set here
+TestKeys.includeTestDependencies := true
 
 // the actual partest the interface calls into -- must be binary version close enough to ours
 // so that it can link to the compiler/lib we're using (testing)
-libraryDependencies += "org.scala-lang.modules" %% "scala-partest" % "1.0-RC5" % "test"
+libraryDependencies ++= (
+  if (TestKeys.includeTestDependencies.value)
+    Seq("org.scala-lang.modules" %% "scala-partest"           % "1.0-RC5" % "test",
+        "org.scala-lang.modules" %% "scala-partest-interface" % "0.2"     % "test")
+  else Seq.empty
+)
+
 
 // necessary for partest -- see comments in its build.sbt
 conflictWarning ~= { _.copy(failOnConflict = false) }

project/keys.scala
@@ -1,0 +1,6 @@
+object TestKeys {
+  import sbt.settingKey
+
+  // for testing with partest
+  val includeTestDependencies = settingKey[Boolean]("Doesn't declare test dependencies.")
+}

commit dd1e0e43f0024b8b06c79fd940ccd253525d02d7

Make partest version a setting
Note: this fixes the breakage introduced by b9695f013a (could not `test`).
Apparently, the order of the test dependencies matters...
(Perhaps because of the conflicting binary version of xml/parsers?)

build.sbt
@@ -81,12 +81,16 @@ pomExtra := (
 // default value must be set here
 TestKeys.includeTestDependencies := true
 
+// default
+TestKeys.partestVersion := "1.0.0-RC6"
+
 // the actual partest the interface calls into -- must be binary version close enough to ours
 // so that it can link to the compiler/lib we're using (testing)
+// NOTE: not sure why, but the order matters (maybe due to the binary version conflicts for xml/parser combinators pulled in for scaladoc?)
 libraryDependencies ++= (
   if (TestKeys.includeTestDependencies.value)
-    Seq("org.scala-lang.modules" %% "scala-partest"           % "1.0-RC5" % "test",
-        "org.scala-lang.modules" %% "scala-partest-interface" % "0.2"     % "test")
+    Seq("org.scala-lang.modules" %% "scala-partest-interface" % "0.2"                         % "test",
+        "org.scala-lang.modules" %% "scala-partest"           % TestKeys.partestVersion.value % "test")
   else Seq.empty
 )
 

project/keys.scala
@@ -3,4 +3,6 @@ object TestKeys {
 
   // for testing with partest
   val includeTestDependencies = settingKey[Boolean]("Doesn't declare test dependencies.")
+
+  val partestVersion = settingKey[String]("Partest version.")
 }