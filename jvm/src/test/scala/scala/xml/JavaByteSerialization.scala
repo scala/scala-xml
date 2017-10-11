830b8b9020c8d3c6f2e5016ec5a641e11bf50cb6
Aaron S. Hawley <aaron.s.hawley@gmail.com>
2017-10-11 19:18:33 -0400

    Make serialization test methods available

--- jvm/src/test/scala/scala/xml/JavaByteSerialization.scala
+++ jvm/src/test/scala/scala/xml/JavaByteSerialization.scala
@@ -4,20 +4,20 @@ import java.io._
 
 object JavaByteSerialization {
   def roundTrip[T](obj: T): T = {
-    def serialize(in: T): Array[Byte] = {
-      val bos = new ByteArrayOutputStream()
-      val oos = new ObjectOutputStream(bos)
-      oos.writeObject(in)
-      oos.flush()
-      bos.toByteArray()
-    }
+    deserialize[T](serialize[T](obj))
+  }
 
-    def deserialize(in: Array[Byte]): T = {
-      val bis = new ByteArrayInputStream(in)
-      val ois = new ObjectInputStream(bis)
-      ois.readObject.asInstanceOf[T]
-    }
+  def serialize[T](in: T): Array[Byte] = {
+    val bos = new ByteArrayOutputStream()
+    val oos = new ObjectOutputStream(bos)
+    oos.writeObject(in)
+    oos.flush()
+    bos.toByteArray()
+  }
 
-    deserialize(serialize(obj))
+  def deserialize[T](in: Array[Byte]): T = {
+    val bis = new ByteArrayInputStream(in)
+    val ois = new ObjectInputStream(bis)
+    ois.readObject.asInstanceOf[T]
   }
 }
