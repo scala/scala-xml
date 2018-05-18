eab3a7970f281c17ec0464b861d02a6ca13f7bfe
Lukas Rytz <lukas.rytz@gmail.com>
2018-05-18 22:03:54 +0200

    Make ScalaVersionSpecific non-public

--- shared/src/main/scala-2.13/scala/xml/ScalaVersionSpecific.scala
+++ shared/src/main/scala-2.13/scala/xml/ScalaVersionSpecific.scala
@@ -5,7 +5,7 @@ import scala.collection.{SeqOps, immutable, mutable}
 import scala.collection.BuildFrom
 import scala.collection.mutable.Builder
 
-object ScalaVersionSpecific {
+private[xml] object ScalaVersionSpecific {
   import NodeSeq.Coll
   type CBF[-From, -A, +C] = BuildFrom[From, A, C]
   object NodeSeqCBF extends BuildFrom[Coll, Node, NodeSeq] {
@@ -14,14 +14,13 @@ object ScalaVersionSpecific {
   }
 }
 
-trait ScalaVersionSpecificNodeSeq
+private[xml] trait ScalaVersionSpecificNodeSeq
   extends SeqOps[Node, immutable.Seq, NodeSeq]
     with StrictOptimizedSeqOps[Node, immutable.Seq, NodeSeq] { self: NodeSeq =>
   override def fromSpecificIterable(coll: Iterable[Node]): NodeSeq = (NodeSeq.newBuilder ++= coll).result()
-
-  override def newSpecificBuilder(): mutable.Builder[Node, NodeSeq] = NodeSeq.newBuilder
+  override def newSpecificBuilder: mutable.Builder[Node, NodeSeq] = NodeSeq.newBuilder
 }
 
-trait ScalaVersionSpecificNodeBuffer { self: NodeBuffer =>
+private[xml] trait ScalaVersionSpecificNodeBuffer { self: NodeBuffer =>
   override def className: String = "NodeBuffer"
 }
