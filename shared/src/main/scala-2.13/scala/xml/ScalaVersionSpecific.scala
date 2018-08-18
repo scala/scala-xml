dc9ce3486f126628ad7f549762a702feaac38c78
Seth Tisue <seth@tisue.net>
2018-08-17 19:11:24 -0700

    Switch to includeOnce in 2.13 Scala-specific code for M5

--- shared/src/main/scala-2.13/scala/xml/ScalaVersionSpecific.scala
+++ shared/src/main/scala-2.13/scala/xml/ScalaVersionSpecific.scala
@@ -1,7 +1,7 @@
 package scala.xml
 
 import scala.collection.immutable.StrictOptimizedSeqOps
-import scala.collection.{SeqOps, immutable, mutable}
+import scala.collection.{SeqOps, IterableOnce, immutable, mutable}
 import scala.collection.BuildFrom
 import scala.collection.mutable.Builder
 
@@ -10,15 +10,14 @@ object ScalaVersionSpecific {
   type CBF[-From, -A, +C] = BuildFrom[From, A, C]
   object NodeSeqCBF extends BuildFrom[Coll, Node, NodeSeq] {
     def newBuilder(from: Coll): Builder[Node, NodeSeq] = NodeSeq.newBuilder
-    def fromSpecificIterable(from: Coll)(it: Iterable[Node]): NodeSeq = (NodeSeq.newBuilder ++= from).result()
+    def fromSpecific(from: Coll)(it: IterableOnce[Node]): NodeSeq = (NodeSeq.newBuilder ++= from).result()
   }
 }
 
 trait ScalaVersionSpecificNodeSeq
   extends SeqOps[Node, immutable.Seq, NodeSeq]
     with StrictOptimizedSeqOps[Node, immutable.Seq, NodeSeq] { self: NodeSeq =>
-  override def fromSpecificIterable(coll: Iterable[Node]): NodeSeq = (NodeSeq.newBuilder ++= coll).result()
-
+  override def fromSpecific(coll: IterableOnce[Node]): NodeSeq = (NodeSeq.newBuilder ++= coll).result()
   override def newSpecificBuilder(): mutable.Builder[Node, NodeSeq] = NodeSeq.newBuilder
 }
 
