61a2691875c518e3564c3552fa004b4df730b810
Aaron S. Hawley <aaron.s.hawley@gmail.com>
2018-09-21 10:08:06 -0400

    Fix exception in XhtmlParser with HTML5 docype
    Make characters, including whitespace, after the `html' identifier in
    <!DOCTYPE html> be optional.  Otherwise, the following errors are
    emitted:
    
        file.xml:2:15: whitespace expected<html xmlns="http://www.w3.org/1999/xhtml">              ^
        file.xml:2:15: document must contain exactly one element
    
    And a java.lang.NullPointerException is thrown.
    
    Unfortunately, testing with Source.fromString doesn't reproduce the
    NullPointerException that occurs with Source.fromFile.

--- jvm/src/test/scala/scala/xml/parsing/ConstructingParserTest.scala
+++ jvm/src/test/scala/scala/xml/parsing/ConstructingParserTest.scala
@@ -4,6 +4,7 @@ package parsing
 import scala.io.Source
 import org.junit.Test
 import scala.xml.JUnitAssertsForXML.{ assertEquals => assertXml }
+import org.junit.Assert.assertEquals
 
 class ConstructingParserTest {
 
@@ -19,4 +20,55 @@ class ConstructingParserTest {
 
   }
 
+  /* Example of using SYSTEM in DOCTYPE */
+  @Test
+  def docbookTest = {
+    val xml =
+      """|<!DOCTYPE docbook SYSTEM 'docbook.dtd'>
+         |<book>
+         |  <title>Book</title>
+         |  <chapter>
+         |    <title>Chapter</title>
+         |    <para>Text</para>
+         |  </chapter>
+         |</book>""".stripMargin
+
+    val expected = <book>
+  <title>Book</title>
+  <chapter>
+    <title>Chapter</title>
+    <para>Text</para>
+  </chapter>
+</book>
+
+    val source = new Source {
+      val iter = xml.iterator
+      override def reportError(pos: Int, msg: String, out: java.io.PrintStream = Console.err) = {}
+    }
+
+    val doc = ConstructingParser.fromSource(source, true).document
+
+    assertEquals(expected, doc.theSeq)
+  }
+
+  /* Unsupported use of lowercase DOCTYPE and SYSTEM */
+  @Test(expected = classOf[scala.xml.parsing.FatalError])
+  def docbookFail: Unit = {
+    val xml =
+      """|<!doctype docbook system 'docbook.dtd'>
+         |<book>
+         |<title>Book</title>
+         |<chapter>
+         |<title>Chapter</title>
+         |<para>Text</para>
+         |</chapter>
+         |</book>""".stripMargin
+
+    val source = new Source {
+      val iter = xml.iterator
+      override def reportError(pos: Int, msg: String, out: java.io.PrintStream = Console.err) = {}
+    }
+
+    ConstructingParser.fromSource(source, true).content(TopScope)
+  }
 }
